a0989be76b293400fe13c106be649e5eadeadbfc
KQUEA402 <KQUEA402@NEWSCHOOL.EDU>
2019-03-05 10:06:30 -0500

    Upload abstract

---
+++ projects/intro/script.js
@@ -0,0 +1,12 @@
+var myName = 'Amaya';
+console.log ('My name is', myName)
+
+var number = '8'
+console.log ('My favorite number is' , number)
+
+var number = 'eight'
+console.log ('My favorite number is' , number)
+
+var  = '8'
+console.log 
+var my_things = [2 + 5, 'samurai', true];

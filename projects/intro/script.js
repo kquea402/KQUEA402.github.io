62147f8b78e069d494d5f33e4e7773c997689bf8
KQUEA402 <KQUEA402@NEWSCHOOL.EDU>
2019-03-06 17:35:51 -0500

    Upload intro

--- projects/intro/script.js
+++ projects/intro/script.js
@@ -4,9 +4,80 @@ console.log ('My name is', myName)
 var number = '8'
 console.log ('My favorite number is' , number)
 
-var number = 'eight'
-console.log ('My favorite number is' , number)
 
 var  = '8'
 console.log 
-var my_things = [2 + 5, 'samurai', true];
+var my_things = [2 + 5, '', true];
+
+var what = true;
+console.log(what)
+
+var nameLetters = ['a','m','a','y','a'];
+console.log("This is how you spell my name:", nameLetters)
+
+var array = [22, 'what', false];
+console.log(array)
+
+var lastName = "Avila";
+var fullName = myName + lastName;
+console.log("My full name is", fullName)
+
+var ten = 10;
+var ninety = 90;
+console.log(ten+ninety)
+
+var firstItem = nameLetters[0];
+var lastItem = nameLetters[nameLetters.length-1];
+console.log("These are the first and last letters of my name:", firstItem, lastItem)
+
+var word = 'tesselate';
+console.log(word.length)
+
+console.log(name.length == lastName.length)
+
+if (i==16) {
+  console.log('If you are a bird')
+} else {
+  console.log("I am a bird")
+}
+
+for (var i = 0; i < nameLetters.length; i++) {
+  console.log(nameLetters[i])
+}
+
+var date = new Date;
+var hour = date.getHours();
+if (hour > 7 && hour < 9.40) {
+  console.log("it's time")
+} else {
+  console.log("nevermind")
+}
+
+var day = date.getDay();
+
+if (day == 1) {
+  console.log("MONDAY?")
+} else if (day== 2) {
+  console.log("TUESDAY?")
+} else if (day == 3) {
+  console.log("WEDNESDAY?")
+} else if (day == 4) {
+  console.log("THURSDAY?")
+} else if (day == 5) {
+  console.log("FRIDAY?")
+} else if (day == 6) {
+  console.log("SATTURDAY")
+} else if (day == 0) {
+  console.log("SUNDAY")
+}
+
+var food = ["bagel","french fries","popcorn"];
+var animal= ["dog","cat", "zebra"];
+var places=["Mexico City", "The passenger seat of my dad's Jeep", "My grandma's kitchen"];
+var pokemon=["pikachu", "luigi", "idk??"];
+
+var allArrays = [food, animal, places, pokemon];
+
+for (var i = 0; i < allArrays.length; i++) {
+  allArrays[i]
+}

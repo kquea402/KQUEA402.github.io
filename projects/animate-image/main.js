6d6fd7816625c3d1b5520d50774a3978ea6a4ca7
KQUEA402 <KQUEA402@NEWSCHOOL.EDU>
2019-03-07 17:12:42 -0500

    Upload vesper

---
+++ projects/animate-image/main.js
@@ -0,0 +1,9 @@
+$(function() {
+  // Your interactions go here
+	$( ".pic" ).click(function() {
+  		$( ".pic" ).animate({ marginLeft: "12in", borderWidth: "10px" }, 1500);
+	});  		
+});
+
+
+
---
+++ projects/toggle-text/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  // Your interactions go here
+$( '.button' ).click(function() {
+	console.log('I clicked it');
+
+$('.words').toggleClass('active');
+});
+});

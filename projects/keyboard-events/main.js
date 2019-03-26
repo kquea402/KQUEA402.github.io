1ce1e0d9cf8c4aa6a05673026faaed11495f0353
KQUEA402 <KQUEA402@NEWSCHOOL.EDU>
2019-03-26 16:39:46 -0400

    Upload clock

--- projects/keyboard-events/main.js
+++ projects/keyboard-events/main.js
@@ -1,3 +1,14 @@
 $(function() {
   // Your interactions go here
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  letters = letters.split("");
+  
+  $(window).keydown(function(event){
+      key = event.which - 65; //makes a-z = 0-27
+      key = letters[key];
+      console.log(key);
+      $('.' + key ).show();
+      // $('.typewriter').append(key);
+  });
 });
+
--- projects/random-boxes/main.js
+++ projects/random-boxes/main.js
@@ -1,3 +1,15 @@
-$(function() {
-  // Your interactions go here
+	$(function() {
+    $('.box').click(function() {
+        console.log("hello")
+      
+        var number=Math.floor((Math.random()
+       *5) +1);
+       console.log(number)
+      
+      $('.one').removeClass('active');
+      $('.one').eq(number).addClass('active');
+   
+  });
 });
+ 
+
